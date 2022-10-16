d536b5bc1a609eeb0b61169fc6f7dda057733087
MarcusWentz <markzcollege404@gmail.com>
2022-10-16 15:04:14 -0400

    Flowrate test script

---
+++ scripts/testTimeOn40mL.js
@@ -0,0 +1,21 @@
+//https://github.com/sarfata/pi-blaster
+//Build and install directly from source
+
+// var piblaster = require('pi-blaster.js');
+
+const timeMilliSec = 1000;
+
+// const pulseWidthMin = 0.05; //SERVO SAFETY VALUE
+const pulseWidthMin = 0.00;    //LED VALUE TO TURN OFF ALL THE WAY
+const pulseWidthMax = 0.35;
+let pulseWidth = pulseWidthMax
+
+const startTime = Date.now();
+
+setInterval(() => {
+
+    // piblaster.setPwm(21, pulseWidth);
+
+    console.log("Seconds passed: " + (Date.now() - startTime)/1000)
+
+}, timeMilliSec);
