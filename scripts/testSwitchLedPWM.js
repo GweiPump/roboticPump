5508202cdd6a62089e4d9147898423de68d928dd
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-10-16 01:36:56 -0400

    Create testSwitchLedPWM.js

---
+++ scripts/testSwitchLedPWM.js
@@ -0,0 +1,18 @@
+//https://github.com/sarfata/pi-blaster
+//Build and install directly from source
+var piblaster = require('pi-blaster.js');
+const timeMilliSec = 2000;
+// const pulseWidthMin = 0.05; //SERVO SAFETY VALUE
+const pulseWidthMin = 0.00;    //LED VALUE TO TURN OFF ALL THE WAY
+const pulseWidthMax = 0.35;
+let pulseWidth = pulseWidthMax
+
+setInterval(() => { 
+  piblaster.setPwm(22, pulseWidth); 
+  console.log(pulseWidth)
+  if (pulseWidth == pulseWidthMin) {
+      pulseWidth = pulseWidthMax
+  } else if (pulseWidth == pulseWidthMax) {
+    	pulseWidth = pulseWidthMin;
+  }
+}, timeMilliSec);
