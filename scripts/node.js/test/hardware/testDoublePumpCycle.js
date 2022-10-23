9473bc0f5310660c6f23eb3075cc188bbff0afe5
MarcusWentz <markzcollege404@gmail.com>
2022-10-23 15:09:00 -0400

    Rename Javascript example

--- scripts/node.js/test/hardware/testDoublePumpCycle.js
+++ scripts/node.js/test/hardware/testDoublePumpCycle.js
@@ -2,8 +2,7 @@
 //Build and install directly from source
 
 // var piblaster = require('pi-blaster.js');
-const halfCycle = 2000;
-const fullCycle = 2*halfCycle;
+const halfCycleMilliseconds = 2000;
 
 // const pulseWidthMin = 0.05; //SERVO SAFETY VALUE
 const pulseWidthMin = 0.00;    //LED VALUE TO TURN OFF ALL THE WAY
@@ -13,20 +12,20 @@ function timeout(ms) {
    return new Promise(resolve => setTimeout(resolve,ms));
  }
 
-testDoubleOnOffCyclePWM()
+testDoublePumpCycle()
 
-async function testDoubleOnOffCyclePWM() {
+async function testDoublePumpCycle() {
 
     // piblaster.setPwm(21, pulseWidth);
     // piblaster.setPwm(23, pulseWidth);
     console.log("ON")
-    await timeout(halfCycle)
-    
+    await timeout(halfCycleMilliseconds)
+
     // piblaster.setPwm(21, pulseWidth);
     // piblaster.setPwm(23, pulseWidth);
     console.log("OFF")
-    await timeout(halfCycle)
+    await timeout(halfCycleMilliseconds)
 
-    testDoubleOnOffCyclePWM()     //Call function again to loop
+    testDoublePumpCycle()     //Call function again to loop
 
 }
