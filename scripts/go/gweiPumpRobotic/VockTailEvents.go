c3b21eddb083648b35b9619a4cf735a83de0862d
MarcusWentz <markzcollege404@gmail.com>
2024-09-20 15:18:06 -0400

    Golang update Base Sepolia contrac with GPIO logic

--- scripts/go/gweiPumpRobotic/VockTailEvents.go
+++ scripts/go/gweiPumpRobotic/VockTailEvents.go
@@ -1,225 +1,225 @@
-//Step 1: Create abi file by running: solc --abi VockTails.sol > VockTails.abi
-//Step 2: Create bin file by running: solc --bin VockTails.sol > VockTails.bin
-//Step 3: Remove comments above the abi and bin files.
-//Step 4: Generate Go contract interaction file by running:  abigen --bin=VockTails.bin --abi=VockTails.abi --pkg=vockTails --out=VockTails.go
-//Step 5: Run: testVockTailEvents.go
-package main
+// //Step 1: Create abi file by running: solc --abi VockTails.sol > VockTails.abi
+// //Step 2: Create bin file by running: solc --bin VockTails.sol > VockTails.bin
+// //Step 3: Remove comments above the abi and bin files.
+// //Step 4: Generate Go contract interaction file by running:  abigen --bin=VockTails.bin --abi=VockTails.abi --pkg=vockTails --out=VockTails.go
+// //Step 5: Run: testVockTailEvents.go
+// package main
 
-import (
+// import (
 
-    "fmt"
-    "log"
-    "os"
-    "context"
-    "math/big"
+//     "fmt"
+//     "log"
+//     "os"
+//     "context"
+//     "math/big"
 
-    vockTails "gweiPumpProject/contracts/VockTails" //LOOK AT "go.mod" FOR YOUR RELATIVE PROJECT PATH TO FIND CONTRACT INTERFACE!
+//     vockTails "gweiPumpProject/contracts/VockTails" //LOOK AT "go.mod" FOR YOUR RELATIVE PROJECT PATH TO FIND CONTRACT INTERFACE!
 
-    "github.com/ethereum/go-ethereum"
-    "github.com/ethereum/go-ethereum/accounts/abi/bind"
-    "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/ethclient"
-    "github.com/ethereum/go-ethereum/core/types"
+//     "github.com/ethereum/go-ethereum"
+//     "github.com/ethereum/go-ethereum/accounts/abi/bind"
+//     "github.com/ethereum/go-ethereum/common"
+//     "github.com/ethereum/go-ethereum/ethclient"
+//     "github.com/ethereum/go-ethereum/core/types"
 
-    "time"
-    "gobot.io/x/gobot"
-    "gobot.io/x/gobot/drivers/gpio"
-    "gobot.io/x/gobot/platforms/raspi"
+//     "time"
+//     "gobot.io/x/gobot"
+//     "gobot.io/x/gobot/drivers/gpio"
+//     "gobot.io/x/gobot/platforms/raspi"
 
-)
+// )
 
-func main() {
+// func main() {
 
-     // Use this endpoint when you are running your own node on a specific chain (no events)
-     // client, chainID := clientSetup("http://localhost:8545")
+//      // Use this endpoint when you are running your own node on a specific chain (no events)
+//      // client, chainID := clientSetup("http://localhost:8545")
 
-     // Use this endpoint when you are running your own node on a specific chain (events allowed)
-     // client, chainID := clientSetup("ws://localhost:8546")
+//      // Use this endpoint when you are running your own node on a specific chain (events allowed)
+//      // client, chainID := clientSetup("ws://localhost:8546")
 
-     client, chainID := clientSetup(os.Getenv("mumbaiQuicknodeWSS"))
+//      client, chainID := clientSetup(os.Getenv("mumbaiQuicknodeWSS"))
 
-     fmt.Println("chainID: ", chainID)
+//      fmt.Println("chainID: ", chainID)
 
-     contractAddress := common.HexToAddress("0xC14708B1faf3737602EA69b68C893beb58baB5a7")
-     contract := connectContractAddress(client,contractAddress)
-     fmt.Println("contract type object: ")
-     fmt.Printf("%T",contract)
-     fmt.Println("")
+//      contractAddress := common.HexToAddress("0xC14708B1faf3737602EA69b68C893beb58baB5a7")
+//      contract := connectContractAddress(client,contractAddress)
+//      fmt.Println("contract type object: ")
+//      fmt.Printf("%T",contract)
+//      fmt.Println("")
 
-     randomDrinkValue := getRandomDrinkValue(contract)
-     fmt.Println("randomDrinkValue:", randomDrinkValue)
+//      randomDrinkValue := getRandomDrinkValue(contract)
+//      fmt.Println("randomDrinkValue:", randomDrinkValue)
 
-     robotJobSinglePumpLeft40mL  := setupRobotJobSinglePumpLeft40mL()
-     robotJobSinglePumpRight40mL := setupRobotJobSinglePumpRight40mL()
-     robotJobDoublePump40mL      := setupRobotJobDoublePump40mL()
+//      robotJobSinglePumpLeft40mL  := setupRobotJobSinglePumpLeft40mL()
+//      robotJobSinglePumpRight40mL := setupRobotJobSinglePumpRight40mL()
+//      robotJobDoublePump40mL      := setupRobotJobDoublePump40mL()
 
-     fmt.Println("Listening for VockTail randomDrinkValue events...")
-     SubscribeToEventsWithRobot(client, contractAddress, contract, robotJobSinglePumpLeft40mL, robotJobSinglePumpRight40mL, robotJobDoublePump40mL)
+//      fmt.Println("Listening for VockTail randomDrinkValue events...")
+//      SubscribeToEventsWithRobot(client, contractAddress, contract, robotJobSinglePumpLeft40mL, robotJobSinglePumpRight40mL, robotJobDoublePump40mL)
 
-}
+// }
 
-func clientSetup(wssConnectionURL string) (client *ethclient.Client, chainID *big.Int) {
+// func clientSetup(wssConnectionURL string) (client *ethclient.Client, chainID *big.Int) {
 
-  client, err := ethclient.Dial(wssConnectionURL)
-  if err != nil {
-      log.Fatal(err)
-  }
+//   client, err := ethclient.Dial(wssConnectionURL)
+//   if err != nil {
+//       log.Fatal(err)
+//   }
 
-  chainID, err = client.NetworkID(context.Background())
-  if err != nil {
-     log.Fatal(err)
-  }
-  return
-}
+//   chainID, err = client.NetworkID(context.Background())
+//   if err != nil {
+//      log.Fatal(err)
+//   }
+//   return
+// }
 
-func connectContractAddress(client *ethclient.Client, contractAddress common.Address) (contract *vockTails.VockTails) {
+// func connectContractAddress(client *ethclient.Client, contractAddress common.Address) (contract *vockTails.VockTails) {
 
-  contract, err := vockTails.NewVockTails(contractAddress, client)
-  if err != nil {
-      log.Fatal(err)
-  }
-  return
-}
+//   contract, err := vockTails.NewVockTails(contractAddress, client)
+//   if err != nil {
+//       log.Fatal(err)
+//   }
+//   return
+// }
 
-func getRandomDrinkValue(contract *vockTails.VockTails) (isPumpFilled *big.Int) {
+// func getRandomDrinkValue(contract *vockTails.VockTails) (isPumpFilled *big.Int) {
 
-  isPumpFilled, err := contract.RandomDrinkValue(&bind.CallOpts{})
-  if err != nil {
-        log.Fatal(err)
-  }
-  return
+//   isPumpFilled, err := contract.RandomDrinkValue(&bind.CallOpts{})
+//   if err != nil {
+//         log.Fatal(err)
+//   }
+//   return
 
-}
+// }
 
-func SubscribeToEventsWithRobot(client *ethclient.Client, contractAddress common.Address, contract *vockTails.VockTails,robotJobSinglePumpLeft40mL *gobot.Robot, robotJobSinglePumpRight40mL *gobot.Robot,robotJobDoublePump40mL *gobot.Robot) {
-  //Subscribe to events from smart contract address.
-  query := ethereum.FilterQuery{
-      Addresses: []common.Address{contractAddress},
-  }
+// func SubscribeToEventsWithRobot(client *ethclient.Client, contractAddress common.Address, contract *vockTails.VockTails,robotJobSinglePumpLeft40mL *gobot.Robot, robotJobSinglePumpRight40mL *gobot.Robot,robotJobDoublePump40mL *gobot.Robot) {
+//   //Subscribe to events from smart contract address.
+//   query := ethereum.FilterQuery{
+//       Addresses: []common.Address{contractAddress},
+//   }
 
-  logs := make(chan types.Log)
-  sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
-  if err != nil {
-      log.Fatal(err)
-  }
+//   logs := make(chan types.Log)
+//   sub, err := client.SubscribeFilterLogs(context.Background(), query, logs)
+//   if err != nil {
+//       log.Fatal(err)
+//   }
 
-  for {
-      select {
-      case err := <-sub.Err():
-          log.Fatal(err)
-      case vLog := <-logs:
-          fmt.Println("New Event Log:", vLog) // pointer to event log
+//   for {
+//       select {
+//       case err := <-sub.Err():
+//           log.Fatal(err)
+//       case vLog := <-logs:
+//           fmt.Println("New Event Log:", vLog) // pointer to event log
 
-          randomDrinkValue, err := contract.RandomDrinkValue(&bind.CallOpts{})
-            if err != nil {
-                log.Fatal(err)
-          }
-          fmt.Println("randomDrinkValue:", randomDrinkValue)
+//           randomDrinkValue, err := contract.RandomDrinkValue(&bind.CallOpts{})
+//             if err != nil {
+//                 log.Fatal(err)
+//           }
+//           fmt.Println("randomDrinkValue:", randomDrinkValue)
 
-          if randomDrinkValue.Cmp(big.NewInt(1)) == 0 {
-              fmt.Println("Gobot: use robotJobSinglePumpLeft40mL")
-              go robotJobSinglePumpLeft40mL.Start() //Use a go routine to keep event listener on when starting gobot.
-          }
-          if randomDrinkValue.Cmp(big.NewInt(2)) == 0 {
-             fmt.Println("Gobot: use robotJobSinglePumpRight40mL")
-              go robotJobSinglePumpRight40mL.Start() //Use a go routine to keep event listener on when starting gobot.
-          }
-          if randomDrinkValue.Cmp(big.NewInt(3)) == 0 {
-              fmt.Println("Gobot: use robotJobDoublePump40mL")
-              go robotJobDoublePump40mL.Start() //Use a go routine to keep event listener on when starting gobot.
-          }
+//           if randomDrinkValue.Cmp(big.NewInt(1)) == 0 {
+//               fmt.Println("Gobot: use robotJobSinglePumpLeft40mL")
+//               go robotJobSinglePumpLeft40mL.Start() //Use a go routine to keep event listener on when starting gobot.
+//           }
+//           if randomDrinkValue.Cmp(big.NewInt(2)) == 0 {
+//              fmt.Println("Gobot: use robotJobSinglePumpRight40mL")
+//               go robotJobSinglePumpRight40mL.Start() //Use a go routine to keep event listener on when starting gobot.
+//           }
+//           if randomDrinkValue.Cmp(big.NewInt(3)) == 0 {
+//               fmt.Println("Gobot: use robotJobDoublePump40mL")
+//               go robotJobDoublePump40mL.Start() //Use a go routine to keep event listener on when starting gobot.
+//           }
 
-          fmt.Println("Listening for VockTails randomDrinkValue events...")
+//           fmt.Println("Listening for VockTails randomDrinkValue events...")
 
-      }
-  }
+//       }
+//   }
 
-  return
-}
+//   return
+// }
 
-func setupRobotJobDoublePump40mL() (robot *gobot.Robot){
+// func setupRobotJobDoublePump40mL() (robot *gobot.Robot){
 
-  r := raspi.NewAdaptor()
-  pump1 := gpio.NewLedDriver(r, "40") //Physical pin 40, GPIO 21.
-  pump2 := gpio.NewLedDriver(r, "16") //Physical pin 16, GPIO 23.
+//   r := raspi.NewAdaptor()
+//   pump1 := gpio.NewLedDriver(r, "40") //Physical pin 40, GPIO 21.
+//   pump2 := gpio.NewLedDriver(r, "16") //Physical pin 16, GPIO 23.
 
-  work := func() {
+//   work := func() {
 
-    fmt.Println("ON!");
+//     fmt.Println("ON!");
 
-    pump1.On();
-    pump2.On();
+//     pump1.On();
+//     pump2.On();
 
-    time.Sleep(24 * time.Second)
+//     time.Sleep(24 * time.Second)
 
-    fmt.Println("OFF!");
-    pump1.Off();
-    pump2.Off();
+//     fmt.Println("OFF!");
+//     pump1.Off();
+//     pump2.Off();
 
-  }
+//   }
 
-  robot = gobot.NewRobot("blinkBot",
-          []gobot.Connection{r},
-          //[]gobot.Device{led},
-          []gobot.Device{pump1,pump2},
-          work,
-  )
+//   robot = gobot.NewRobot("blinkBot",
+//           []gobot.Connection{r},
+//           //[]gobot.Device{led},
+//           []gobot.Device{pump1,pump2},
+//           work,
+//   )
 
-  return
-}
+//   return
+// }
 
-func setupRobotJobSinglePumpLeft40mL() (robot *gobot.Robot){
+// func setupRobotJobSinglePumpLeft40mL() (robot *gobot.Robot){
 
-  r := raspi.NewAdaptor()
-  pump1 := gpio.NewLedDriver(r, "40") //Physical pin 40, GPIO 21.
+//   r := raspi.NewAdaptor()
+//   pump1 := gpio.NewLedDriver(r, "40") //Physical pin 40, GPIO 21.
 
-  work := func() {
+//   work := func() {
 
-    fmt.Println("ON!");
+//     fmt.Println("ON!");
 
-    pump1.On();
+//     pump1.On();
 
-    time.Sleep(45 * time.Second)
+//     time.Sleep(45 * time.Second)
 
-    fmt.Println("OFF!");
-    pump1.Off();
+//     fmt.Println("OFF!");
+//     pump1.Off();
 
-  }
+//   }
 
-  robot = gobot.NewRobot("blinkBot",
-          []gobot.Connection{r},
-          //[]gobot.Device{led},
-          []gobot.Device{pump1},
-          work,
-  )
+//   robot = gobot.NewRobot("blinkBot",
+//           []gobot.Connection{r},
+//           //[]gobot.Device{led},
+//           []gobot.Device{pump1},
+//           work,
+//   )
 
-  return
-}
+//   return
+// }
 
-func setupRobotJobSinglePumpRight40mL() (robot *gobot.Robot){
+// func setupRobotJobSinglePumpRight40mL() (robot *gobot.Robot){
 
-  r := raspi.NewAdaptor()
-  pump2 := gpio.NewLedDriver(r, "16") //Physical pin 16, GPIO 23.
+//   r := raspi.NewAdaptor()
+//   pump2 := gpio.NewLedDriver(r, "16") //Physical pin 16, GPIO 23.
 
-  work := func() {
+//   work := func() {
 
-    fmt.Println("ON!");
+//     fmt.Println("ON!");
 
-    pump2.On();
+//     pump2.On();
 
-    time.Sleep(45 * time.Second)
+//     time.Sleep(45 * time.Second)
 
-    fmt.Println("OFF!");
-    pump2.Off();
+//     fmt.Println("OFF!");
+//     pump2.Off();
 
-  }
+//   }
 
-  robot = gobot.NewRobot("blinkBot",
-          []gobot.Connection{r},
-          //[]gobot.Device{led},
-          []gobot.Device{pump2},
-          work,
-  )
+//   robot = gobot.NewRobot("blinkBot",
+//           []gobot.Connection{r},
+//           //[]gobot.Device{led},
+//           []gobot.Device{pump2},
+//           work,
+//   )
 
-  return
-}
+//   return
+// }
